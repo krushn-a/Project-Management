718d9e6ca433568bf7c0cad6cbbc3e0d0224aa2e
Krushna <codeofkrushna@gmail.com>
2025-12-20 09:39:36 +0530

    backend complete

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import workSpaceRouter from './routes/workSpaceRoute.js';
 import { protect } from './middlewares/authMiddleware.js';
+import projectRouter from "./routes/projectRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
 
 const app = express();
 
@@ -20,6 +23,10 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 //routes
 app.use('/api/workspaces', protect, workSpaceRouter);
+app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
+app.use("/api/comments", protect, commentRouter);
+
 
 const PORT = process.env.PORT || 5000;
 
