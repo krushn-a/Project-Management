cf247c4fc0204673dff29ec4dcbe9acbe2152609
Krushna <codeofkrushna@gmail.com>
2025-12-20 09:49:26 +0530

    corrected typos

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProject, addMember, updateProject } from "../controllers/projectController";
+import { createProject, addMember, updateProject } from "../controllers/projectController.js";
 
 
 const projectRouter = express.Router();
